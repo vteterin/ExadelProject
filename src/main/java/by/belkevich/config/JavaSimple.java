d6b98fc5abd0c0bd01285fe12527e0371ae50320
Viktor Teterin <viktor.teterin@gmail.com>
2018-12-12 19:59:20 +0300

    Update JavaSimple.java

--- src/main/java/by/belkevich/config/JavaSimple.java
+++ src/main/java/by/belkevich/config/JavaSimple.java
@@ -12,7 +12,21 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import java.io.IOException;
 
+
 public class JavaSimple {
+  static class ExampleServlet extends HttpServlet {
+    static final Counter requests = Counter.build()
+        .name("hello_worlds_total")
+        .help("Number of hello worlds served.").register();
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
+        throws ServletException, IOException {
+      resp.getWriter().println("Hello World!");
+      // Increment the number of requests.
+      requests.inc();
+    }
+  }
 
   public static void main( String[] args ) throws Exception {
       Server server = new Server(1234);
@@ -26,5 +40,9 @@ public class JavaSimple {
       // Add metrics about CPU, JVM memory etc.
       DefaultExports.initialize();
 
+
+      // Start the webserver.
+      server.start();
+      server.join();
   }
 }
